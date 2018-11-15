d6af571c829716a173d0fee4ad56839d04a976b0
Ricardo Tosin <tosinricardo@gmail.com>
2018-11-15 19:51:53 -0200

    changes to api
    changes to api exception handler

--- app/Phone.php
+++ app/Phone.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Phone extends Model
 {
-    //
+    //To provide away to seed the database ! 
     protected $fillable = ['nome','ddd','phone_number'];
 }
--- routes/api.php
+++ routes/api.php
@@ -13,15 +13,25 @@ use Illuminate\Http\Request;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Route::get('phones',function(){
+//     return response(Phones::all(),200);
 // });
 
-Route::get('phones',function(){
-    return response(Phones::all(),200);
-});
-
-Route::get('phones/{phones}', function($phoneID){
-    return response(Phone::find($phoneID),200);
+// Route::get('phones/{phones}', function($phoneID){
+//     return response(Phone::find($phoneID),200);
     
-});
+// });
+
+
+// Now the products are injected in Controller, and Laravel Implicity binds it
+
+Route::get('phones','PhonesController@index');
+Route::get('phones/{phone}','PhonesController@show');
+Route::post('phones','PhonesController@store');
+Route::put('phones/{phone}','PhonesController@update');
+Route::delete('phones/{phone}','PhonesController@delete');
+
