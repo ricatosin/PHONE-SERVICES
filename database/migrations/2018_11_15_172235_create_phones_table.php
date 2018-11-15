95ca35e5adcd88daa34ffbb7ba297c94c2622914
Ricardo Tosin <tosinricardo@gmail.com>
2018-11-15 17:03:10 -0200

    Initiate Project
    Firte Commit !

---
+++ database/migrations/2018_11_15_172235_create_phones_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('nome',20);
+            $table->string('ddd',3);//->unsigned();
+            $table->string('phone_number',10);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+}
